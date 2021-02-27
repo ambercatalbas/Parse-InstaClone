282809a60a75edef1095591cda6cd8cf39590edc
sifactlbs <sifa.catalbas@keove.com>
2021-02-27 16:22:57 +0300

    Initial Commit

---
+++ Parse InstaClone/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Parse InstaClone
+//
+//  Created by Yasemin YEL on 27.02.2021.
+//  Copyright © 2021 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Parse InstaClone/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Parse InstaClone
+//
+//  Created by Yasemin YEL on 27.02.2021.
+//  Copyright © 2021 Sifa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
